e24636c98bc1c3641c08d708105e15070d2dcb22
vicxke <vic.heirman@gmail.com>
2025-09-29 10:18:57 +0200

    Lab_03_task_01_finished

--- Lab 1/files/lab_03.sv
+++ Lab 1/files/lab_03.sv
@@ -18,6 +18,22 @@ module lab_03;
         endtask;
     endclass: Test1
 
+    class Task1;
+        task run;
+            $display("\nTask 1");
+
+            #10 $display("Main - thread @ line %0d @ time %0t", `__LINE__, $time);
+            #20 $display("Main - thread @ line %0d @ time %0t", `__LINE__, $time);
+
+            fork
+                #60 $display("Fork - thread 1 @ line %0d @ time %0t", `__LINE__, $time);
+            join
+
+            #10 $display("Main - thread @ line %0d @ time %0t", `__LINE__, $time);
+            #100 $display("Main - thread @ line %0d @ time %0t", `__LINE__, $time);
+        endtask;
+    endclass: Task1
+
 
     class Test2;
         task run;
@@ -102,6 +118,35 @@ module lab_03;
         endtask;
     endclass: Test5
 
+        
+    class Task5;
+        task run;
+            $display("\nTask 5");
+
+            #10 $display("Main - thread @ line %0d @ time %0t", `__LINE__, $time);
+            #20 $display("Main - thread @ line %0d @ time %0t", `__LINE__, $time);
+
+            fork
+                #60 $display("Fork - thread 1 @ line %0d @ time %0t", `__LINE__, $time);
+                #10 $display("Fork - thread 2 @ line %0d @ time %0t", `__LINE__, $time);
+            join_any
+        
+            fork
+                begin
+                    #10 $display("Fork2 - thread 3 @ line %0d @ time %0t", `__LINE__, $time);
+                    #20 $display("Fork2 - thread 3 @ line %0d @ time %0t", `__LINE__, $time);
+                    #80 $display("Fork2 - thread 3 @ line %0d @ time %0t", `__LINE__, $time);
+                end
+            join_none
+
+            #10 $display("Main - thread @ line %0d @ time %0t", `__LINE__, $time);
+
+            wait fork;
+
+            #100 $display("Main - thread @ line %0d @ time %0t", `__LINE__, $time);            
+        endtask;
+    endclass: Task5
+
 
     initial begin
         Test1 test1;
@@ -109,10 +154,15 @@ module lab_03;
         Test3 test3;
         Test4 test4;
         Test5 test5;
+        Task1 task1;         // Task 1
+
 
 
         test1 = new();
         test1.run();
+
+        task1 = new();        // Task 1
+        task1.run();          // Task 1
 
         test2 = new();
         test2.run();
@@ -126,6 +176,7 @@ module lab_03;
         test5 = new();
         test5.run();
 
+        
 
         // Task 1
         // Create a new class named "Task1" that is a carbon copy of the "Test1" class with the exception that the "fork-join" block only creates 1 thread.
