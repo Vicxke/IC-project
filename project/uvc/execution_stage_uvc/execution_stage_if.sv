90df8d02e50b0715ec07465914016ee5b8c97a4f
Vicxke <60980080+Vicxke@users.noreply.github.com>
2025-11-09 16:10:44 +0100

    succesfull conpile and run

--- project/uvc/execution_stage_uvc/execution_stage_if.sv
+++ project/uvc/execution_stage_uvc/execution_stage_if.sv
@@ -14,4 +14,12 @@ interface execution_stage_if (input logic clk, input logic rst_n);
     logic compflg_in;
     logic [31:0] program_counter;
 
+    // outputs from dut (made available to monitors/scoreboard)
+    //not shure if it should be here
+    control_type control_out;
+    logic [31:0] alu_data;
+    logic [31:0] memory_data;
+    logic overflow_flag;
+    logic compflg_out;
+
 endinterface : execution_stage_if
