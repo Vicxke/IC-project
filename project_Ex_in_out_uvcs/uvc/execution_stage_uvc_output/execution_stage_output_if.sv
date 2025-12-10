ef44ee210fd71b45dd75d6bbc7cc3ed5a7e8f960
vicxke <vic.heirman@gmail.com>
2025-12-10 14:05:34 +0100

    seq_item changes

--- project_Ex_in_out_uvcs/uvc/execution_stage_uvc_output/execution_stage_output_if.sv
+++ project_Ex_in_out_uvcs/uvc/execution_stage_uvc_output/execution_stage_output_if.sv
@@ -6,13 +6,6 @@ interface execution_stage_output_if (input logic clk, input logic rst_n);
     // Import DUT common typedefs (control_type, encodings, etc.)
     import common::*;
 
-    // inputs to dut.
-    logic [31:0] data1;
-    logic [31:0] data2;
-    logic [31:0] immediate_data;
-    control_type control_in;
-    logic compflg_in;
-    logic [31:0] program_counter;
 
     // outputs from dut (made available to monitors/scoreboard)
     //not shure if it should be here
--- project_Ex_in_out_uvcs/uvc/execution_stage_uvc_output/execution_stage_output_seq_item.svh
+++ project_Ex_in_out_uvcs/uvc/execution_stage_uvc_output/execution_stage_output_seq_item.svh
@@ -25,16 +25,25 @@ class execution_stage_output_seq_item extends uvm_sequence_item;
 
     // expected outputs (for scoreboard checks)
     logic [31:0] alu_data;
+    logic [31:0] memory_data;
     bit overflow_flag;
     bit zero_flag;
-    // monitor helper flag
-    bit monitor_data_valid;
+    control_type control_out;
+    logic compflg_in;
+    logic [31:0] program_counter;
+
+
+
 
     // Fields for printing/packing
     `uvm_object_utils_begin(execution_stage_output_seq_item)
     `uvm_field_int(alu_data, UVM_ALL_ON)
+    `uvm_field_int(memory_data, UVM_ALL_ON)
     `uvm_field_int(overflow_flag, UVM_ALL_ON)
     `uvm_field_int(zero_flag, UVM_ALL_ON)
+    `uvm_field_object(control_out, UVM_ALL_ON)
+    `uvm_field_int(compflg_in, UVM_ALL_ON)
+    `uvm_field_int(program_counter, UVM_ALL_ON)
     `uvm_object_utils_end
 
     // Constructor
