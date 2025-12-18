b81a6de12f0993150d7c8b71cbce4706ec36cb1a
Tim Bein <tim.bein@tum.de>
2025-12-18 18:26:35 +0100

    critical line

--- project_Execution_Decode/Ex_De_Stage/tb/tb_top.sv
+++ project_Execution_Decode/Ex_De_Stage/tb/tb_top.sv
@@ -240,12 +240,12 @@ module tb_top;
     // Start UVM test_base environment
     initial begin // only one run valid
         // run_test("ExDeStage_00");
-        //run_test("ExDeStage_01"); 
-        //run_test("ExDeStage_02"); 
+        run_test("ExDeStage_01"); 
+        // run_test("ExDeStage_02"); 
         // run_test("ExDeStage_03");
         //run_test("ExDeStage_04");
         // run_test("ExDeStage_05");
-        run_test("ExDeStage_06");
+        // run_test("ExDeStage_06");
 
     end
 
